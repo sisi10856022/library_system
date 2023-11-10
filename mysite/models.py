7385c3322b478fa6d281f0b2f7d8287e72867eed
sisi10856022 <10856022@ntub.edu.tw>
2023-11-10 22:36:00 +0800

    InitProject

---
+++ mysite/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.CharField(max_length=200)
+    body = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-pub_date',)
+
+    def __str__(self):
+        return self.title
+    
+class Product(models.Model):
+    SIZES = (
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+    )
+    sku = models.CharField(max_length=5)
+    name = models.CharField(max_length=20)
+    price = models.PositiveIntegerField()
+    size = models.CharField(max_length=1, choices=SIZES)
+    result = models.BooleanField()
